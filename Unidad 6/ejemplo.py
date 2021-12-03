75f3740224f28bcc1cbb2ec56b59a79a4a243342
5t4t1ck <dsaavedra88@gmail.com>
2021-12-03 17:11:35 -0500

    Ejemplo de listas, tuplas y diccionarios

---
+++ Unidad 6/ejemplo.py
@@ -0,0 +1,21 @@
+def funcion():
+    list("a","b","c","d")
+    
+print(list[1])
+lista = [2,"a","Juan", True]
+lista2 = [1,2,3,4]
+lista3 = [True, True, True]
+lista4 = [False, False, False]
+
+print(len(lista))
+print(min(lista2))
+print(max(lista2))
+print(sum(lista2))
+
+print("dato" in lista2)
+print(lista2.index(4))
+print(lista2.index(4))
+print(all(lista3))
+print(all(lista4))
+print(any(lista3))
+print(any(lista4))
